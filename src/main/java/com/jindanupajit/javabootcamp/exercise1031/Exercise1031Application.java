ca8b3eec9c00eb38f8fa63412b402f12c570c588
Krissada Jindanupajit <jindanupajit@gmail.com>
2019-10-07 14:35:37 -0400

    Exercise 103 1

---
+++ src/main/java/com/jindanupajit/javabootcamp/exercise1031/Exercise1031Application.java
@@ -0,0 +1,13 @@
+package com.jindanupajit.javabootcamp.exercise1031;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exercise1031Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Exercise1031Application.class, args);
+    }
+
+}
